63a04390a48c719adab570915b461e4192109812
Unknown <MXCapturing@gmail.com>
2019-10-01 15:25:19 +1300

    Initial Push and Planet Rotation

---
+++ Personal Project/Assets/_Scripts/FauxGravityBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+    public Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        _rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
---
+++ Personal Project/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector2 moveDir;
+    public Rigidbody2D _rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), 0).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 globalMoveDir = (transform.TransformDirection(moveDir));
+        _rb.MovePosition(_rb.position + globalMoveDir * speed * Time.deltaTime);
+    }
+}
